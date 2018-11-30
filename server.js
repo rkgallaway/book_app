139fdb80c194a19471e093ab634e3f2fb24fbd35
tyler <tyler.r.hood@gmail.com>
2018-11-30 12:23:13 -0800

    added head and header and footer .ejs

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ let allBooks = [];
 function Book(info) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
   this.title = info.volumeInfo.title || 'No title available';
-  this.isbn = info.volumeInfo.industryIdentifiers[0].identifier || info.volumeInfo.industryIdentifiers[1].identifier|| 'No ISBN available';
+  this.isbn = info.volumeInfo.industryIdentifiers[0].identifier || info.volumeInfo.industryIdentifiers[1].identifier || 'No ISBN available';
   this.image_url = info.volumeInfo.imageLinks.smallThumbnail|| placeholderImage;
   this.author = info.volumeInfo.authors || 'No author available';
   this.description = info.volumeInfo.description || 'No description available';
@@ -47,24 +47,19 @@ function handleError(err, res) {
 function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-  console.log('~~~~~~~~~~~~~~~~~~');
-  console.log('request.body: ', request.body);
+  // console.log('~~~~~~~~~~~~~~~~~~');
+  // console.log('request.body: ', request.body);
 
-  if (request.body.searchRadio === 'title') { url += `+intitle:${request.body.searchTerm}&maxResults=1`; }
+  if (request.body.searchRadio === 'title') { url += `+intitle:${request.body.searchTerm}&maxResults=3`; }
   if (request.body.searchRadio === 'author') { url += `+inauthor:${request.body.searchTerm}`; }
 
-  console.log('url: ' , url);
-  console.log('~~~~~~~~~~~~~~~~~~');
+  // console.log('url: ' , url);
+  // console.log('~~~~~~~~~~~~~~~~~~');
   superagent.get(url)
-    .then(apiResponse => {
-      apiResponse.body.items.map(bookResult => new Book(bookResult));
-      console.log('apiResponse.body.items[0].volumeInfo.title: ', apiResponse.body.items[0].volumeInfo.title);
-      console.log('apiResponse.body.items[0].volumeInfo.industryIdentifiers[0].identifier: ' , apiResponse.body.items[0].volumeInfo.industryIdentifiers[0].identifier);
-      console.log('apiResponse.body.items[0].volumeInfo.imageLinks.smallThumbnail: ', apiResponse.body.items[0].volumeInfo.imageLinks.smallThumbnail);
-      console.log('apiResponse.body.items[0].volumeInfo.authors[0]: ', apiResponse.body.items[0].volumeInfo.authors[0]);
-      console.log('apiResponse.body.items[0].volumeInfo.description: ', apiResponse.body.items[0].volumeInfo.description);
-    })
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult)))
+
     .then(results => {
+      console.log(`results.title: ${results}`);
       response.render('pages/searches/show', {searchResults: results})
     })
     .catch(error => handleError(error, response));
